983ce89f2210884a2156491d1613e2fca57238f1
Will Hanighen <willplays2022@gmail.com>
2025-05-14 03:28:41 -0400

    test a thig

--- auth.py
+++ auth.py
@@ -35,7 +35,7 @@ google_sso = GoogleSSO(
     client_id=os.getenv("GOOGLE_CLIENT_ID", ""),
     client_secret=os.getenv("GOOGLE_CLIENT_SECRET", ""),
     redirect_uri=os.getenv("GOOGLE_REDIRECT_URI", ""),
-    allow_insecure_http=False
+    allow_insecure_http=True
 )
 
 # Dependency to get the DB session
